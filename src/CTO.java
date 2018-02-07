9dac32f43f6ad57a4e3ac0f8c8d4dd5f00da5fdd
Ken Li <iliawnek@gmail.com>
2018-02-07 20:59:27 +0000

    Clean up CTO and KPT

--- src/CTO.java
+++ src/CTO.java
@@ -6,44 +6,51 @@ public class CTO {
 
     public static void main(String[] args) {
         String filepath = args[0];
+
+        // read cipher text from file
+        ArrayList<String> cipherText = new ArrayList<>();
         try {
-            // read cipher text from file
-            ArrayList<String> cipherText = new ArrayList<>();
             BufferedReader reader = new BufferedReader(new FileReader(filepath));
             String line = null;
             while ((line = reader.readLine()) != null) cipherText.add(line);
-
-            // brute force search
-            double keyCount = Math.pow(2, 16);
-            String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890., \n"; // assumed
-
-            for (int key = 1; key < keyCount; key++) {
-                String message = "";
-                boolean skip = false;
-                for (String block : cipherText) {
-                    // decrypt block
-                    int decrypted = Coder.decrypt(key, Integer.decode(block));
-
-                    // first char
-                    int c0 = decrypted / 256;
-                    if (alphabet.indexOf(c0) < 0) {skip = true; break;}
-                    message += (char) c0;
-
-                    // second char
-                    int c1 = decrypted % 256;
-                    if (c1 != 0) {
-                        if (alphabet.indexOf(c1) < 0) {skip = true; break;}
-                        message += (char) c1;
-                    }
-                }
-                // print only if key results in a message constructed using assumed alphabet
-                if (!skip) {
-                    System.out.printf("key = 0x%04X\n", key);
-                    System.out.printf("message = %s\n", message);
-                }
-            }
         } catch (Exception e) {
             e.printStackTrace();
+        }
+
+        // brute force search
+        int keyCount = 65536;
+        String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz., \n"; // assumed
+
+        for (int key = 0; key < keyCount; key++) {
+            String message = "";
+            boolean skip = false;
+            for (String block : cipherText) {
+                // decrypt block
+                int decrypted = Coder.decrypt(key, Integer.decode(block));
+
+                // first char
+                int c0 = decrypted / 256;
+                if (alphabet.indexOf(c0) < 0) {
+                    skip = true;
+                    break;
+                }
+                message += (char) c0;
+
+                // second char
+                int c1 = decrypted % 256;
+                if (c1 != 0) {
+                    if (alphabet.indexOf(c1) < 0) {
+                        skip = true;
+                        break;
+                    }
+                    message += (char) c1;
+                }
+            }
+            // print only if key results in a message constructed using assumed alphabet
+            if (!skip) {
+                System.out.printf("key = 0x%04X\n", key);
+                System.out.printf("message = %s\n", message);
+            }
         }
     }
 }
--- src/KPT.java
+++ src/KPT.java
@@ -3,9 +3,9 @@ public class KPT {
     public static void main(String[] args) {
         int cipherText = Integer.decode("0xb73f");
         int plainText = Integer.decode("0x4120");
-        double keyCount = Math.pow(2, 16);
+        int keyCount = 65536;
 
-        for (int key = 1; key < keyCount; key++) {
+        for (int key = 0; key < keyCount; key++) {
             int decrypted = Coder.decrypt(key, cipherText);
             if (decrypted == plainText) System.out.printf("0x%04X", key);
         }

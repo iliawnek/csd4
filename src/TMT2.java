5a691a135deebfb9a5b887a4ac943de4f0c9829a
Ken Li <iliawnek@gmail.com>
2018-02-07 21:01:40 +0000

    Start TMT2

--- src/TMT2.java
+++ src/TMT2.java
@@ -1,14 +1,36 @@
-/**
- * Given a full cipher text, this program will use a time memory tradeoff
- * table to obtain the first cipher text block, discover the key, then
- * use the key to decode the rest of the cipher text.
- *
- * The time memory tradeoff table will be supplied via a text file.
- */
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
 
 public class TMT2 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int cipherText = Integer.decode("0x5b1b");
+
+        // read table
+        String tableFilepath = args[1];
+        HashMap<Integer, Integer> table = new HashMap<>();
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(tableFilepath));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] splitLine = line.split(",");
+                int end = Integer.parseInt(splitLine[0]);
+                int start = Integer.parseInt(splitLine[1]);
+                table.put(end, start);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // check if cipher text is end of a chain
+        if (table.get(cipherText) != null) {
+
+        }
+        // check if cipher text is in middle of chain
+        else {
+
+        }
     }
 }

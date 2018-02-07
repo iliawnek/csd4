8fd5dc7954fc5d337ccc93737d56e286e9916c25
Ken Li <iliawnek@gmail.com>
2018-02-07 19:26:31 +0000

    Find key and message for CTO attack

--- src/CTO.java
+++ src/CTO.java
@@ -1,16 +1,49 @@
-/**
- * Given only a chunk of cipher text, this program will perform a brute force attack.
- *
- * Once the key has been found, this program will use the key to decode the rest of
- * the message.
- *
- * This program will also count how many cipher text blocks are needed to decode the
- * message unambiguously.
- */
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
 
 public class CTO {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        String filepath = args[0];
+        try {
+            // read cipher text from file
+            ArrayList<String> cipherText = new ArrayList<>();
+            BufferedReader reader = new BufferedReader(new FileReader(filepath));
+            String line = null;
+            while ((line = reader.readLine()) != null) cipherText.add(line);
+
+            // brute force search
+            double keyCount = Math.pow(2, 16);
+            String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890., \n"; // assumed
+
+            for (int key = 1; key < keyCount; key++) {
+                String message = "";
+                boolean skip = false;
+                for (String block : cipherText) {
+                    // decrypt block
+                    int decrypted = Coder.decrypt(key, Integer.decode(block));
+
+                    // first char
+                    int c0 = decrypted / 256;
+                    if (alphabet.indexOf(c0) < 0) {skip = true; break;}
+                    message += (char) c0;
+
+                    // second char
+                    int c1 = decrypted % 256;
+                    if (c1 != 0) {
+                        if (alphabet.indexOf(c1) < 0) {skip = true; break;}
+                        message += (char) c1;
+                    }
+                }
+                // print only if key results in a message constructed using assumed alphabet
+                if (!skip) {
+                    System.out.printf("key = 0x%04X\n", key);
+                    System.out.printf("message = %s\n", message);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }

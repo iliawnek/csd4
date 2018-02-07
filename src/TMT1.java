f896456528e86c5b8eb7af6d57b8914ad4a7cd57
Ken Li <iliawnek@gmail.com>
2018-02-07 21:01:28 +0000

    Finish TMT1

--- src/TMT1.java
+++ src/TMT1.java
@@ -1,13 +1,29 @@
-/**
- * Given only a plain text block, this program will construct a time memory
- * tradeoff table.
- *
- * The table will be written to a text file.
- */
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Random;
 
 public class TMT1 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        String filepath = args[0];
+        int plainText = Integer.decode("0x5365");
+        int keyCount = 65536;
+        int chainLength = 256;
+        int chainCount = 256;
+        Random random = new Random();
+
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter(filepath));
+            for (int i = 0; i < chainCount; i++) {
+                int start = random.nextInt(keyCount);
+                int end = start;
+                for (int j = 0; j < chainLength; j++) end = Coder.encrypt(end, plainText);
+                writer.write(String.format("%d,%d\n", end, start));
+            }
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
